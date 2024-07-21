e8f9391b4b71be78775cf52f75010f4b2082323c
정민 <jmeve24@naver.com>
2024-07-21 10:31:04 +0900

    🎉 스프링부트 환경설정

---
+++ backend/src/main/java/com/egginhealth/EgginhealthApplication.java
@@ -0,0 +1,13 @@
+package com.egginhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgginhealthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EgginhealthApplication.class, args);
+    }
+
+}

13b635251d329330fe181c36fe0f89de25622e0a
신재건 <guneleven@gmail.com>
2024-07-23 17:42:29 +0900

    :wrench: #S11P12C203-165 테일윈드, 주스탄드 초기세팅 완료

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; // 테일윈드 CSS 파일 임포트
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);

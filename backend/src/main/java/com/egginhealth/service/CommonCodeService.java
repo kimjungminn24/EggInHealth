106727954adca3a58a5dd81d8abe85d69fb9ad0f
민주 <ks00919@naver.com>
2024-07-25 17:06:45 +0900

    :recycle: #S11P12C203-196 공통 코드 서비스 객체 @Transactional 제거
    조회만 담당하기 때문에 제거했어요

--- backend/src/main/java/com/egginhealth/service/CommonCodeService.java
+++ backend/src/main/java/com/egginhealth/service/CommonCodeService.java
@@ -2,14 +2,12 @@ package com.egginhealth.service;
 
 import com.egginhealth.data.dto.CommonCodeDto;
 import com.egginhealth.data.repository.CommonCodeRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class CommonCodeService {
 

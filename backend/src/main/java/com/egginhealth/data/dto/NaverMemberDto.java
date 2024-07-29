f72e6fc30fd17dc202fe6f7e57348a80e68ced26
정민 <jmeve24@naver.com>
2024-07-29 20:53:33 +0900

    ✨ #S11P12C203-203 네이버에서 멤버 정보 가져오기
    - AuthService
      - OAuth2 로그인 요청을 처리
      - 사용자 정보를 가져와서 변환
    - NaverMemberDto
      - 네이버 사용자 정보
      - 스프링 시큐리티가 로그인 처리를 위해 필요한 객체를 위한 메서드 구현

---
+++ backend/src/main/java/com/egginhealth/data/dto/NaverMemberDto.java
@@ -0,0 +1,44 @@
+package com.egginhealth.data.dto;
+
+import com.egginhealth.data.entity.Role;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+@Builder
+public class NaverMemberDto {
+
+    private String name;
+    private String email;
+    private String imgUrl;
+    private String phoneNumber;
+    private Role role;
+    private String provider;
+    private String key;
+
+
+    public static NaverMemberDto from(Map<String, Object> response) {
+        return NaverMemberDto.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .imgUrl((String) response.get("profile_image"))
+                .phoneNumber((String) response.get("phone_number"))
+                .role(Role.NONE)
+                .provider("naver")
+                .build();
+    }
+
+    public static Map<String, Object> convertMap(Map<String, Object> response) {
+        return Map.of(
+                "id", "naver",
+                "key", "naver",
+                "name", response.get("name"),
+                "email", response.get("email"),
+                "imgUrl", response.get("profile_image"),
+                "phoneNumber", response.get("phone_number"),
+                "role", Role.NONE
+        );
+    }
+}

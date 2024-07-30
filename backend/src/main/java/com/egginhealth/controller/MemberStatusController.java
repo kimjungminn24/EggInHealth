118fa152e21652b122eae950d3130833363bfc1c
민주 <ks00919@naver.com>
2024-07-30 09:55:45 +0900

    :bulb: 주석 삭제

--- backend/src/main/java/com/egginhealth/controller/MemberStatusController.java
+++ backend/src/main/java/com/egginhealth/controller/MemberStatusController.java
@@ -16,8 +16,6 @@ public class MemberStatusController {
 
     private final MemberStatusService memberStatusService;
 
-    //
-
     @GetMapping("/{uid}")
     public ResponseEntity<List<MemberStatusDto>> getMonthlyMemberStatus(@PathVariable("uid") int id, @RequestParam("year") int year, @RequestParam("month") int month) {
         return new ResponseEntity<>(memberStatusService.getMemberStatusByMonth(id, year, month), HttpStatus.OK);

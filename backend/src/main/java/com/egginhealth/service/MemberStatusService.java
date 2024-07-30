b910db2f5c21651975e41f7f3ed61c056c3c8c4c
민주 <ks00919@naver.com>
2024-07-30 09:50:37 +0900

    :construction: 회원 집계 저장 메서드 구현 중 업로드

--- backend/src/main/java/com/egginhealth/service/MemberStatusService.java
+++ backend/src/main/java/com/egginhealth/service/MemberStatusService.java
@@ -1,6 +1,7 @@
 package com.egginhealth.service;
 
-import com.egginhealth.data.dto.MemberStatusDto;
+import com.egginhealth.data.dto.memberstatus.MemberStatusDto;
+import com.egginhealth.data.dto.memberstatus.MemberStatusInputDto;
 import com.egginhealth.data.repository.MemberStatusRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,11 @@ import java.util.List;
 public class MemberStatusService {
 
     private final MemberStatusRepository memberStatusRepository;
+
+    // TODO: 회원 집계 저장 메서드 구현
+    public void saveMemberStatus(MemberStatusInputDto memberStatusInputDto) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
     public List<MemberStatusDto> getMemberStatusByMonth(int memberId, int year, int month) {
         return memberStatusRepository.findByMemberIdAndYearAndMonth(memberId, year, month).stream()

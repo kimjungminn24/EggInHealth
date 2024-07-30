8b6d3f94537aca32ddbab46ba611143a7f6ec100
정민 <jmeve24@naver.com>
2024-07-30 16:47:50 +0900

    ✨ 목표 등록 API 구현
    - POST /api/goal
    - 성공시 CREATED 응답
    - GoalDto 추가

---
+++ backend/src/main/java/com/egginhealth/controller/GoalController.java
@@ -0,0 +1,35 @@
+package com.egginhealth.controller;
+
+
+import com.egginhealth.data.dto.GoalDto;
+import com.egginhealth.service.GoalService;
+import com.egginhealth.util.SecurityUtil;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/goal")
+@RequiredArgsConstructor
+public class GoalController {
+
+    private final GoalService goalService;
+    private final SecurityUtil securityUtil;
+
+
+    @PostMapping
+    public ResponseEntity<Void> postGoalBy(@RequestBody @Valid GoalDto goalDto) {
+
+        goalService.saveGoal(goalDto, securityUtil.getUserId());
+        return new ResponseEntity<>(HttpStatus.CREATED);
+
+    }
+
+}
---
+++ backend/src/main/java/com/egginhealth/data/dto/GoalDto.java
@@ -0,0 +1,11 @@
+package com.egginhealth.data.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+public record GoalDto(
+        @NotNull int exerciseCommonId,
+        @NotNull int dietCommonId,
+        @NotNull int commonId
+) {
+
+}

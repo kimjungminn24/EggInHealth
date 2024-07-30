ca435cc75d2fc01148412a7077df059087130138
민주 <ks00919@naver.com>
2024-07-30 15:04:21 +0900

    :sparkles: #S11P12C203-237 체형 입력 폼 DTO 작성

---
+++ backend/src/main/java/com/egginhealth/data/dto/bodycomposition/BodyCompositionInputDto.java
@@ -0,0 +1,18 @@
+package com.egginhealth.data.dto.bodycomposition;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+
+public record BodyCompositionInputDto(
+        BigDecimal height,
+        BigDecimal weight,
+        BigDecimal muscle,
+        BigDecimal fat,
+        BigDecimal bmi,
+        BigDecimal compositionScore,
+        MultipartFile image,
+        int memberId
+) {
+
+}

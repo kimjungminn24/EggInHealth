3115907b8a31c49e115c24079fb2d80bc7210f26
gun <guneleven@gmail.com>
2024-07-31 23:29:13 +0900

    :poop: #S11P12C203-132 프로필까지 임시완성

--- frontend/src/components/user/UserPage.jsx
+++ frontend/src/components/user/UserPage.jsx
@@ -1,9 +1,22 @@
-const UserPage = ()=>{
-    return(
-        <div>
-            내정보임
-        </div>
-    )
-}
+import React from "react";
+import useStore from "../../store/store_test";
 
-export default UserPage
+const UserPage = () => {
+  const profile = useStore((state) => state.profile);
+const goal = useStore((state)=>state.goal)
+  return (
+    <div>
+      <p>이름: {profile.Name}</p>
+      <p>전화번호: {profile.PhoneNumber}</p>
+      <p>트레이너 ID: {profile.TrainerId}</p>
+      <p>나이: {profile.Age}</p>
+      <p>PT 횟수: {profile.PtCnt}</p>
+      <p>타입: {profile.Type}</p>
+    <p>운동 강도 : {goal.ExerciseCommonId}</p>
+    <p>식단조절 : {goal.DietCommonId}</p>
+    <p>목표 : {goal.CommonId}</p>
+    </div>
+  );
+};
+
+export default UserPage;

770aec21994582697f5fc3b02838b3a9dd0838f4
정민 <jmeve24@naver.com>
2024-08-01 22:34:53 +0900

    ✨ #S11P12C203-272 운동 계획 등록 API 구현

--- backend/src/main/java/com/egginhealth/controller/ExerciseController.java
+++ backend/src/main/java/com/egginhealth/controller/ExerciseController.java
@@ -1,16 +1,14 @@
 package com.egginhealth.controller;
 
 
+import com.egginhealth.data.dto.exercise.ExerciseInputDto;
 import com.egginhealth.data.dto.exercise.ExerciseReportInputDto;
 import com.egginhealth.service.ExerciseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -22,6 +20,13 @@ public class ExerciseController {
 
     private final ExerciseService exerciseService;
 
+    @PostMapping()
+    public ResponseEntity<Void> postExerciseSetBy(@RequestBody ExerciseInputDto inputData) {
+
+        exerciseService.saveExerciseSet(inputData);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+
+    }
 
     @PostMapping("/report")
     public ResponseEntity<Void> postExerciseReportBy(@ModelAttribute ExerciseReportInputDto inputData) throws IOException {
@@ -30,4 +35,6 @@ public class ExerciseController {
         return new ResponseEntity<>(HttpStatus.CREATED);
 
     }
+
+
 }

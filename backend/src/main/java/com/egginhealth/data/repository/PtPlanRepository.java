168556cc7742374cb5fb7d79c478270c2c9baccd
hkk3626 <hkk3626@naver.com>
2024-08-02 17:42:49 +0900

    :sparkles: #BE/feat/S11P12C203-284 PT일정조회 기능API 작성

---
+++ backend/src/main/java/com/egginhealth/data/repository/PtPlanRepository.java
@@ -0,0 +1,20 @@
+package com.egginhealth.data.repository;
+
+import com.egginhealth.data.entity.PtPlan;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface PtPlanRepository extends JpaRepository<PtPlan,Integer> {
+
+    @Query("SELECT pp FROM PtPlan pp WHERE pp.member.id = :memberId AND YEAR(pp.date) = :year AND MONTH(pp.date) = :month ORDER BY pp.date ASC" )
+    List<PtPlan> findByMemberId(int memberId, int year, int month);
+
+    @Query("SELECT pp FROM PtPlan pp WHERE pp.member.id = :memberId AND pp.date > :specificDate ORDER BY pp.date ASC" )
+    List<PtPlan> findByMemberTopNow(int memberId, LocalDateTime specificDate, Pageable pageable);
+}

4f5d2794d9714ba8e7c4bc08afb16fba8952e910
민주 <ks00919@naver.com>
2024-08-06 10:55:06 +0900

    :wrench: #S11P12C203-314 output 파일경로 설정

--- ai/config.py
+++ ai/config.py
@@ -3,4 +3,5 @@ import os
 
 class Config:
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
+    OUTPUT_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'outputs')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB

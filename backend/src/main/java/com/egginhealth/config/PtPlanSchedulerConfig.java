2b70baa1de25a9035d19b8b219dca4b0105db190
정민 <jmeve24@naver.com>
2024-08-06 14:31:49 +0900

    ✨ #S11P12C203-320 스프링 스케줄러 설정 추가

---
+++ backend/src/main/java/com/egginhealth/config/PtPlanSchedulerConfig.java
@@ -0,0 +1,22 @@
+package com.egginhealth.config;
+
+import com.egginhealth.service.PTPlanService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class PtPlanSchedulerConfig {
+
+    private final PTPlanService ptPlanService;
+
+    @Scheduled(cron = "0 0/30 * * * *")
+    public void ptPlanCheck() {
+        log.info("Decrease PT Plan Count");
+        ptPlanService.decreasePtPlanCount();
+    }
+
+}

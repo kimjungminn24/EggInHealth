d595ac82ac056be22e25cf60db83e75fd9aece90
hkk3626 <hkk3626@naver.com>
2024-08-06 17:53:54 +0900

    :wrench: #S11P12C203-322 안쓰는 import구문 삭제

--- backend/src/main/java/com/egginhealth/service/MemberService.java
+++ backend/src/main/java/com/egginhealth/service/MemberService.java
@@ -5,7 +5,6 @@ import com.egginhealth.data.entity.Member;
 import com.egginhealth.data.entity.Role;
 import com.egginhealth.data.repository.MemberRepository;
 import com.egginhealth.util.SecurityUtil;
-import com.egginhealth.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;

315941fa1b29711d8a36da2984583236d4a2da9c
정민 <jmeve24@naver.com>
2024-08-07 11:30:45 +0900

    ♻️  #S11P12C203-324 deprecated된 메서드명 수정

--- backend/src/main/java/com/egginhealth/EgginhealthApplication.java
+++ backend/src/main/java/com/egginhealth/EgginhealthApplication.java
@@ -3,11 +3,11 @@ package com.egginhealth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class EgginhealthApplication {
 
     public static void main(String[] args) {

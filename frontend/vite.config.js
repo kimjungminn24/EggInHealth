7208c4bbd8b06c885f98f7bf7dfcd15cc1c2a524
hkk3626 <hkk3626@naver.com>
2024-08-08 09:11:33 +0900

    🔧 #S11P12C203-330 icon 파일 변경 및 위치이동

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from "vite-plugin-pwa";
@@ -16,40 +15,45 @@ export default defineConfig({
         start_url: '/',
         icons: [
             {
-                "src": 'src/assets/icons/maskable_icon_x48.png',
-                "sizes": "48x48",
+                "src": "icons/apple-touch-icon-57x57.png",
+                "sizes": "57x57",
                 "type": "image/png"
             },
             {
-                "src": 'src/assets/icons/maskable_icon_x72.png',
-                "sizes": '72x72',
-                "type": "image/png"
-            },
-            {
-                "src": 'src/assets/icons/maskable_icon_x96.png',
-                "sizes": "96x96",
-                "type": "image/png"
-            },
-            {
-                "src": 'src/assets/icons/maskable_icon_x128.png',
-                "sizes": "128x128",
-                "type": "image/png"
-            },
-            {
-              "src": 'src/assets/icons/maskable_icon_x192.png',
-              "sizes": '192x192',
+              "src": "icons/apple-touch-icon-60x60.png",
+              "sizes": "60x60",
               "type": "image/png"
             },
             {
-                "src": 'src/assets/icons/maskable_icon_x384.png',
-                "sizes": "384x384",
-                "type": "image/png"
+              "src": "icons/apple-touch-icon-72x72.png",
+              "sizes": "72x72",
+              "type": "image/png"
             },
             {
-                "src": 'src/assets/icons/maskable_icon_x512.png',
-                "sizes": "512x512",
-                "type": "image/png"
-            }
+              "src": "icons/apple-touch-icon-76x76.png",
+              "sizes": "76x76",
+              "type": "image/png"
+            },
+            {
+              "src": "icons/apple-touch-icon-114x114.png",
+              "sizes": "114x114",
+              "type": "image/png"
+            },
+            {
+              "src": "icons/apple-touch-icon-120x120png",
+              "sizes": "120x120",
+              "type": "image/png"
+            },
+            {
+              "src": "icons/apple-touch-icon-144x144.png",
+              "sizes": "144x144",
+              "type": "image/png"
+            },
+            {
+              "src": "icons/apple-touch-icon-152x152.png",
+              "sizes": "152x152",
+              "type": "image/png"
+            }           
         ],
       }, 
     })

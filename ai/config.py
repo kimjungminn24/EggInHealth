8fd372ab79d2bfa143cb5bc2e6d5a2c4277fd328
민주 <ks00919@naver.com>
2024-08-09 16:32:27 +0900

    :bento: #S11P12C203-326 폰트 파일 업로드

--- ai/config.py
+++ ai/config.py
@@ -4,5 +4,5 @@ import os
 class Config:
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
     OUTPUT_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'outputs')
-    FONT_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'fonts')
+    FONT_FILE_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'fonts')
     MAX_CONTENT_LENGTH = 500 * 1024 * 1024  # 500MB

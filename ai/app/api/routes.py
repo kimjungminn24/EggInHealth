9a63dd5282493039293b970cc35ead557f2c3ed2
Minju <69795199+ks00919@users.noreply.github.com>
2024-08-11 16:21:44 +0900

    :sparkles: #S11P12C203-344 api 파라미터로 받도록 수정

--- ai/app/api/routes.py
+++ ai/app/api/routes.py
@@ -2,6 +2,7 @@ import os
 
 from flask import jsonify, request, Blueprint, current_app, send_from_directory
 from werkzeug.utils import secure_filename
+
 from app.model.motion_detector import detect
 
 video_bp = Blueprint('video_api', __name__)
@@ -20,9 +21,11 @@ def upload_video():
     if file.filename == '':
         return jsonify({'error': 'No file selected'}), 400
 
+    mode = request.args.get('mode')
+
     if file and allowed_file(file.filename):
         file_name = secure_filename(file.filename)
         file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], file_name))
-        detect(file_name)
+        detect(file_name, mode)
         return send_from_directory(current_app.config['OUTPUT_FOLDER'], file_name, as_attachment=True)
     return jsonify({'error': 'Invalid file type'}), 400

e203a8aa1dc2825bc8062688bb5de67afce6fa26
Minju <69795199+ks00919@users.noreply.github.com>
2024-08-11 16:19:07 +0900

    :sparkles: #S11P12C203-344 운동 분기 처리

--- ai/app/model/motion_detector.py
+++ ai/app/model/motion_detector.py
@@ -1,8 +1,12 @@
-from app.model.processors import squat_processor
-from flask import Flask, current_app
+from flask import Flask
+
+from app.model.processors import biceps_curl_processor, squat_processor
 
 app = Flask(__name__)
 
 
-def detect(file_name):
-    squat_processor.detect(file_name)
+def detect(file_name, mode):
+    if mode == 0:
+        squat_processor.detect(file_name)
+    elif mode == 1:
+        biceps_curl_processor.detect(file_name)

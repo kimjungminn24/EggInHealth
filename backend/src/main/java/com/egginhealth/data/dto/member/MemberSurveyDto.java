7da46683e766d1dbd1d8ff176ad36ca177e2b0c4
정민 <jmeve24@naver.com>
2024-08-12 15:25:30 +0900

    ✨ #S11P12C203-352 성별 추가

--- backend/src/main/java/com/egginhealth/data/dto/member/MemberSurveyDto.java
+++ backend/src/main/java/com/egginhealth/data/dto/member/MemberSurveyDto.java
@@ -4,7 +4,8 @@ import jakarta.validation.constraints.NotNull;
 
 public record MemberSurveyDto(
         @NotNull int age,
-        @NotNull int height
+        @NotNull int height,
+        @NotNull String gender
 ) {
 
 }

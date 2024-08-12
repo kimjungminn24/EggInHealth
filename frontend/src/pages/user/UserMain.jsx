e2377443a291a13cf13b50658600e1e5d98e3d0f
kochungwon <yud01063@gmail.com>
2024-08-12 22:49:20 +0900

    :art: #S11P12C203-135 모든 api 연결 완료

--- frontend/src/pages/user/UserMain.jsx
+++ frontend/src/pages/user/UserMain.jsx
@@ -27,18 +27,18 @@ const Message = styled.div`
 `;
 
 const ScrollableContainer = styled.div`
-  max-height: 400px; /* Adjust height as needed */
+  max-height: 400px; 
   overflow-y: auto;
 `;
 
-// Helper function to format time in 12-hour AM/PM notation
+
 const formatTime = (date) => {
   let hours = date.getHours();
   const minutes = String(date.getMinutes()).padStart(2, '0');
   const ampm = hours >= 12 ? 'PM' : 'AM';
 
   hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
+  hours = hours ? hours : 12; 
 
   return `${hours}:${minutes} ${ampm}`;
 };
@@ -72,14 +72,12 @@ const UserMain = () => {
     if (trainer) {
       userSchedule(userId)
         .then(response => {
-          console.log(response);
           const convertedTimebox = response.map(schedule => {
             const startDate = new Date(schedule.startTime);
             const endDate = new Date(schedule.endTime);
     
             const formattedDate = `${startDate.getMonth() + 1}.${startDate.getDate()}(${['일', '월', '화', '수', '목', '금', '토'][startDate.getDay()]})`;
             
-            // Format start and end times
             const formattedStartTime = formatTime(startDate);
             const formattedEndTime = formatTime(endDate);
             

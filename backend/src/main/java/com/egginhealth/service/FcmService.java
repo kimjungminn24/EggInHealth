f7ae3598497fb4b9861aa613178d14d775bd6104
hkk3626 <hkk3626@naver.com>
2024-08-14 11:15:34 +0900

    :adhesive_bandage: #S11P12C203-370 JSON 파싱 수정

--- backend/src/main/java/com/egginhealth/service/FcmService.java
+++ backend/src/main/java/com/egginhealth/service/FcmService.java
@@ -9,11 +9,9 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
-import io.github.cdimascio.dotenv.Dotenv;
-import jakarta.annotation.PostConstruct;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.stereotype.Service;
@@ -24,65 +22,40 @@ import java.nio.charset.StandardCharsets;
 
 
 @Service
-@RequiredArgsConstructor
 @EnableRedisRepositories
 public class FcmService {
-
-    @Value("${FCM_TYPE}")
-    private String type;
-
-    @Value("${FCM_PROJECT_ID}")
-    private String projectId;
-
-    @Value("${FCM_PRIVATE_KEY_ID}")
-    private String privateKeyId;
-
-    @Value("${FCM_PRIVATE_KEY}")
-    private String privateKey;
-
-    @Value("${FCM_CLIENT_EMAIL}")
-    private String clientEmail;
-
-    @Value("${FCM_CLIENT_ID}")
-    private String clientId;
-
-    @Value("${FCM_AUTH_URI}")
-    private String authUri;
-
-    @Value("${FCM_TOKEN_URI}")
-    private String tokenUri;
-
-    @Value("${FCM_AUTH_PROVIDER_X509_CERT_URL}")
-    private String authProviderX509CertUrl;
-
-    @Value("${FCM_CLIENT_X509_CERT_URL}")
-    private String clientX509CertUrl;
-
-    @Value("${FCM_UNIVERSE_DOMAIN}")
-    private String universeDomain;
-
 
     private static final Logger logger = LoggerFactory.getLogger(FcmService.class);
 
     private final FcmRepository fcmRepository;
-    private final Dotenv dotenv = Dotenv.load();
 
-
-    public DeviceToken getDeviceToken(String id) {
-        return fcmRepository.findById(id).orElse(null);
+    @Autowired
+    public FcmService(@Value("${FCM_TYPE}") String type,
+                      @Value("${FCM_PROJECT_ID}") String projectId,
+                      @Value("${FCM_PRIVATE_KEY_ID}") String privateKeyId,
+                      @Value("${FCM_PRIVATE_KEY}") String privateKey,
+                      @Value("${FCM_CLIENT_EMAIL}") String clientEmail,
+                      @Value("${FCM_CLIENT_ID}") String clientId,
+                      @Value("${FCM_AUTH_URI}") String authUri,
+                      @Value("${FCM_TOKEN_URI}") String tokenUri,
+                      @Value("${FCM_AUTH_PROVIDER_X509_CERT_URL}") String authProviderX509CertUrl,
+                      @Value("${FCM_CLIENT_X509_CERT_URL}") String clientX509CertUrl,
+                      @Value("${FCM_UNIVERSE_DOMAIN}") String universeDomain, FcmRepository fcmRepository) throws IOException {
+        this.fcmRepository = fcmRepository;
+        initialize(type, projectId, privateKeyId, privateKey, clientEmail, clientId, authUri, tokenUri, authProviderX509CertUrl, clientX509CertUrl, universeDomain);
     }
 
-    public void saveDeviceToken(String id, String token) {
-        try {
-            DeviceToken deviceToken = new DeviceToken(id, token);
-            fcmRepository.save(deviceToken);
-        } catch (Exception ex) {
-            logger.error("saveDeviceToken error. {}", ex.toString());
-        }
-    }
-
-    @PostConstruct
-    public void initialize() throws IOException {
+    private void initialize(String type,
+                            String projectId,
+                            String privateKeyId,
+                            String privateKey,
+                            String clientEmail,
+                            String clientId,
+                            String authUri,
+                            String tokenUri,
+                            String authProviderX509CertUrl,
+                            String clientX509CertUrl,
+                            String universeDomain) throws IOException {
 
         String firebaseConfigJson = String.format(
                 "{ \"type\": \"%s\", \"project_id\": \"%s\", \"private_key_id\": \"%s\", \"private_key\": \"%s\", \"client_email\": \"%s\", \"client_id\": \"%s\", \"auth_uri\": \"%s\", \"token_uri\": \"%s\", \"auth_provider_x509_cert_url\": \"%s\", \"client_x509_cert_url\": \"%s\", \"universe_domain\": \"%s\" }",
@@ -97,6 +70,20 @@ public class FcmService {
 
         if (FirebaseApp.getApps().isEmpty()) {
             FirebaseApp.initializeApp(options);
+        }
+    }
+
+
+    public DeviceToken getDeviceToken(String id) {
+        return fcmRepository.findById(id).orElse(null);
+    }
+
+    public void saveDeviceToken(String id, String token) {
+        try {
+            DeviceToken deviceToken = new DeviceToken(id, token);
+            fcmRepository.save(deviceToken);
+        } catch (Exception ex) {
+            logger.error("saveDeviceToken error. {}", ex.toString());
         }
     }
 
@@ -119,7 +106,7 @@ public class FcmService {
             String response = FirebaseMessaging.getInstance().send(message);
             logger.info("Successfully sent message: {}", response);
         } catch (Exception ex) {
-            logger.info("Failed sent message: {}", ex);
+            logger.info("Failed sent message: {}", ex.toString());
         }
     }
 }

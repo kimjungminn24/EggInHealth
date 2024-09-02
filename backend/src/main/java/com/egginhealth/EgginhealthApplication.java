1fdc1f6e7e593ea9ccb69fa2116281733da84537
정민 <jmeve24@naver.com>
2024-09-02 21:30:34 +0900

    ➕ feign 라이브러리 추가

--- backend/src/main/java/com/egginhealth/EgginhealthApplication.java
+++ backend/src/main/java/com/egginhealth/EgginhealthApplication.java
@@ -2,12 +2,14 @@ package com.egginhealth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableMethodSecurity
+@EnableFeignClients
 public class EgginhealthApplication {
 
     public static void main(String[] args) {
